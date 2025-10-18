d0c0804dfcbf6ea84b2da525a87439a0ecb4c6e7
amer-devops <amerahmed.official@gmail.com>
2025-10-18 21:51:50 +0530

    terraform

---
+++ count-loops/variables.tf
@@ -0,0 +1,12 @@
+variable "instances" {
+ default = ["mongodb" , "catalogue" , "redis"] 
+}
+
+variable "zone_id" {
+  default = "Z09921652HC7E9W0W018V"
+}
+
+variable "domain_name" {
+    default = "daws86s.icu"
+  
+}
