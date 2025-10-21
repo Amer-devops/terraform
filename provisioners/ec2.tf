ec38d5f17a652398ab5a6d79e91e60a7470bbf1e
amer-devops <amerahmed.official@gmail.com>
2025-10-21 13:14:42 +0530

    terraform

---
+++ provisioners/ec2.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    tags={
+        Name = "terraform"
+        Terraform = "true"
+    }
+
+    provisioner "local-exec" {
+      command = "echo ${self.private_ip} > inventory "
+      on_failure = continue # when provisioner "local-exec" fails it will continue by using this command
+    }
+
+    provisioner "local-exec" {
+      command = "echo instance is destroyed"
+      when = destroy
+      
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "allow-all-v1"
+
+   egress {
+    from_port        = 0 #from_port "0" to to_port "0" means all ports
+    to_port          = 0
+    protocol         = "-1" #-1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from_port "0" to to_port "0" means all ports
+    to_port          = 0
+    protocol         = "-1" # "-1" means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+  tags ={
+    Name = "allow-all-v1"
+  }
+}
+
+
+ 
